e12193351b269aac49cc1e2c34daca39c47ea726
unknown <cristianromano2@gmail.com>
2019-05-15 00:01:16 -0300

    ADD BOOTSTRAP AND JOBS

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from .models import Job
+
 def home(request):
-    jobs = Job.objects #va a agarrar todos los objectos de la database y convertirlos en objeto de python
-    return render(request ,'jobs/home.html')
+    jobs = Job.objects 
+    return render(request ,'jobs/home.html' , {'jobs':jobs})
