46017eaf0e88df982c4a910262db33fd09684a43
unknown <cristianromano2@gmail.com>
2019-05-14 21:18:58 -0300

    ss

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Job
+def home(request):
+    jobs = Job.objects #va a agarrar todos los objectos de la database y convertirlos en objeto de python
+    return render(request ,'jobs/home.html')
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'vlog.apps.VlogConfig',
     'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
